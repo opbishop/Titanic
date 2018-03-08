df02f7e791c17f8445243588bf6436fc6214865f
Oli Bishop <oliverbishop90@gmail.com>
2018-03-08 23:00:24 +0000

    pep8 standards

--- data_models/passenger.py
+++ data_models/passenger.py
@@ -1,4 +1,4 @@
-class Passenger():
+class Passenger:
     def __init__(self, last_name, first_name, age, boarded, survived):
         self.last_name = last_name
         self.first_name = first_name
--- data_processor.py
+++ data_processor.py
@@ -11,6 +11,7 @@ titles = {
 # 'Sir', 'Major', 'Ms', 'Father', 'Colonel', 'Mme.', 'Mlle'}
 # I'm being optimistic and assuming dr, reverend could be either...
 
+
 def populate(soup):
     passengers = []
     sections = soup.find_all('div', {'class': 'sixteen columns'})
@@ -20,9 +21,10 @@ def populate(soup):
         class_passengers = []
         for person in people:
             last_name, first_name, age, boarded, survived = [identifier.text for identifier in person.contents]
+            # Accounting for weird unicode characters
             first_name = first_name.replace('Â', '')
             try:
-                p = passenger.Passenger(last_name, first_name, int(age), boarded, survived=='S')
+                p = passenger.Passenger(last_name, first_name, int(age), boarded, survived == 'S')
 
                 for k, v in titles.items():
                     if first_name.split()[0] in v:
@@ -33,21 +35,23 @@ def populate(soup):
             except ValueError:
                 pass
 
-
         passengers.append(class_passengers)
 
     titanic = ship.Ship('Titanic', passengers)
 
     return titanic
 
+
 def analyse(dataset):
     df = DataFrame()
     for data in dataset:
-        df1 = DataFrame([person.get_values() for person in data], columns=['Surname', 'First name', 'Age', 'Boarded', 'Survived', 'Gender'])
+        df1 = DataFrame([person.get_values() for person in data],
+                        columns=['Surname', 'First name', 'Age', 'Boarded', 'Survived', 'Gender'])
         df1['Class'] = [dataset.index(data)+1]*len(data)
         df = pd.concat([df, df1], ignore_index=True)
 
     survived_by_class(df)
+
 
 def survived_by_class(df):
     df = pd.crosstab(df.Class, df.Survived).apply(lambda r: r/r.sum(), axis=1)
